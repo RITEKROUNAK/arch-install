944578d748565cb9974b22df5c53dccd7871b47d
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-15 07:16:23 +0530

    install-arch: Setup method to prepare partition for install

--- install-arch.sh
+++ install-arch.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+BOOT_PARTITION=/dev/sda1
+ROOT_PARTITION=/dev/sda2
+
 pre_install () {
 	echo "Enable network time"
 	timedatectl set-ntp true
 }
 
+prepare_partitions () {
+	echo "Formatting paritionts..."
+	mkfs.fat -F32 $BOOT_PARTITION
+	mkfs.ext4 $ROOT_PARTITION
+	echo "Mounting partitions..."
+	mount $ROOT_PARTITION /mnt
+	mkdir /mnt/boot
+	mount $BOOT_PARTITION /mnt/boot
+}
+
 echo "Performing pre install tasks..."
 pre_install
+echo "Done"
+
+echo "Preparing paritions..."
+prepare_partitions
 echo "Done"
